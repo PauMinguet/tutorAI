745f5ac43cf52cc32bb992b3e1adc4f1c54c5600
PauMinguet <pablitumm@gmail.com>
2024-04-07 14:11:13 -0700

    firs6

---
+++ BACKEND/src/api/viewDocuments.py
@@ -0,0 +1,37 @@
+import json
+import os
+from fastapi import APIRouter, Depends
+#from src.api import auth
+import sqlalchemy
+from src import database as db
+from operator import itemgetter
+from sqlalchemy.exc import DBAPIError
+from pydantic import BaseModel
+import requests
+
+router = APIRouter(
+    prefix="/viewDocuments",
+    tags=["viewDocuments"],
+    #dependencies=[Depends(auth.get_api_key)],
+)
+
+
+@router.get("/")
+def searchVectorDB():
+
+    
+    try:
+        with db.engine.begin() as connection:
+            results = connection.execute(sqlalchemy.text("""select distinct name, source, author, link from items""")).fetchall()
+
+        print(results)
+        results = [dict(zip(["name", "source", "author", "link"], result)) for result in results]
+
+        
+        return {"results": results}
+    
+    except DBAPIError as error:
+     
+        print(f"Error returned: <<<{error}>>>")
+    
+
